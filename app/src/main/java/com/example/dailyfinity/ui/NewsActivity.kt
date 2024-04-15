9a53eac966937cd663d4d7e20e16da023c59aef8
coder <soumyodipsutra@gmail.com>
2024-04-15 09:25:21 +0530

    made fragment_saved_news.xml design

--- app/src/main/java/com/example/dailyfinity/ui/NewsActivity.kt
+++ app/src/main/java/com/example/dailyfinity/ui/NewsActivity.kt
@@ -2,15 +2,24 @@ package com.example.dailyfinity.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.dailyfinity.R
 
 class NewsActivity : AppCompatActivity() {
 
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+        navController = findNavController(R.id.newsNavHostFragment)
+        setupActionBarWithNavController(navController)
 
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onNavigateUp()
     }
 }
