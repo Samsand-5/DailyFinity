579526c75dca9809116233342294b66908b76008
coder <soumyodipsutra@gmail.com>
2024-05-12 14:58:32 +0530

    making retrofit class

--- app/src/main/java/com/example/dailyfinity/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/dailyfinity/api/RetrofitInstance.kt
@@ -1,7 +1,11 @@
 package com.example.dailyfinity.api
 
+import com.example.dailyfinity.db.NewsApi
+import com.example.dailyfinity.util.Constants.Companion.BASE_URL
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
     companion object{
@@ -12,7 +16,15 @@ class RetrofitInstance {
                 .addInterceptor(logging)
                 .build()
 
-            Ret
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+        }
+
+        val api by lazy {
+            retrofit.create(NewsApi::class.java)
         }
     }
 }
