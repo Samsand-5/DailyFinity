ec8161681b516db443ed826423213b3d49de07a6
coder <soumyodipsutra@gmail.com>
2024-05-12 14:49:02 +0530

    api

---
+++ app/src/main/java/com/example/dailyfinity/db/NewsApi.kt
@@ -0,0 +1,25 @@
+package com.example.dailyfinity.db
+
+import com.example.dailyfinity.models.NewsResponse
+import com.example.dailyfinity.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("v2/top-headlines")
+    suspend fun getHeadLines(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+
+    )
+}
