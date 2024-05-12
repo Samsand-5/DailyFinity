24398cea61831dbbff907241a30b8a163aa8364d
coder <soumyodipsutra@gmail.com>
2024-05-12 17:11:31 +0530

    creating room database and making DAO which is a interface helps in CRUD(Create, Read, Update, Delete) operation. It is basically abstract methods for interacting with dataBase.
    Selecting articles method Created.

--- app/src/main/java/com/example/dailyfinity/db/ArticleDao.kt
+++ app/src/main/java/com/example/dailyfinity/db/ArticleDao.kt
@@ -1,8 +1,10 @@
 package com.example.dailyfinity.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.dailyfinity.models.Article
 
 @Dao
@@ -10,4 +12,7 @@ interface ArticleDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
 }
