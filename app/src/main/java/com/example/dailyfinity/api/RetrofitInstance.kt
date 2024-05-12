f184d9028bf6810f5628aa834027ca574219e692
coder <soumyodipsutra@gmail.com>
2024-05-12 14:55:49 +0530

    making retrofit class

--- app/src/main/java/com/example/dailyfinity/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/dailyfinity/api/RetrofitInstance.kt
@@ -1,7 +1,18 @@
 package com.example.dailyfinity.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
 class RetrofitInstance {
     companion object{
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
 
+            Ret
+        }
     }
 }
