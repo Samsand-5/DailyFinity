38d11dd766c222d3f34d0ec7b9dd79cb03dbe9d9
coder <soumyodipsutra@gmail.com>
2024-05-12 17:09:56 +0530

    creating room database and making DAO which is a interface helps in CRUD(Create, Read, Update, Delete) operation. It is basically abstract methods for interacting with dataBase.
    Insert method Created.

--- app/src/main/java/com/example/dailyfinity/db/ArticleDao.kt
+++ app/src/main/java/com/example/dailyfinity/db/ArticleDao.kt
@@ -1,4 +1,13 @@
 package com.example.dailyfinity.db
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.dailyfinity.models.Article
+
+@Dao
 interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
 }
