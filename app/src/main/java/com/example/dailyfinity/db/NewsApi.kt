92fc4ed026a084f0ad64439e2754e720bda6789a
coder <soumyodipsutra@gmail.com>
2024-05-12 14:51:23 +0530

    api

--- app/src/main/java/com/example/dailyfinity/db/NewsApi.kt
+++ app/src/main/java/com/example/dailyfinity/db/NewsApi.kt
@@ -21,6 +21,10 @@ interface NewsApi {
     @GET("v2/everything")
     suspend fun searchForNews(
         @Query("q")
-        searchQuery: String
-    )
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY
+    ): Response<NewsResponse>
 }
