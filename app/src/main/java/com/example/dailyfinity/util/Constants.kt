351f2b60aaa003c6f123aaa407a534c7bf992513
coder <soumyodipsutra@gmail.com>
2024-05-12 14:39:34 +0530

    added Constants.kt

---
+++ app/src/main/java/com/example/dailyfinity/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.dailyfinity.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "b3a51abb898d41ac8584930d4f30928e"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
