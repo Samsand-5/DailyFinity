2821b5bf484e2e386a476f7e28df3a56f7214635
coder <soumyodipsutra@gmail.com>
2024-05-14 18:45:59 +0530

    making article databse

--- app/src/main/java/com/example/dailyfinity/db/ArticleDatabase.kt
+++ app/src/main/java/com/example/dailyfinity/db/ArticleDatabase.kt
@@ -1,5 +1,6 @@
 package com.example.dailyfinity.db
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
@@ -11,6 +12,23 @@ import com.example.dailyfinity.models.Article
 )
 
 @TypeConverters(Converters::class)
-class ArticleDatabase {
+abstract class ArticleDatabase {
 
+    abstract fun getArticleDao(): ArticleDao
+
+    companion object{
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance?: synchronized(LOCK){
+            instance?: createDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun createDatabase(context: Context): Any {
+
+        }
+    }
 }
