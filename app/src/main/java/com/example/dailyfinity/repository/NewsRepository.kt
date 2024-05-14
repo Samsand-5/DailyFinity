7a0351a79d6892205b20ae651c706a9f38766075
coder <soumyodipsutra@gmail.com>
2024-05-14 18:57:48 +0530

    making news repository

--- app/src/main/java/com/example/dailyfinity/repository/NewsRepository.kt
+++ app/src/main/java/com/example/dailyfinity/repository/NewsRepository.kt
@@ -8,8 +8,11 @@ import com.example.dailyfinity.models.Article
 
 class NewsRepository(val db: ArticleDatabase) {
 
-    suspend fun getHeadLines(countryCode: String, pageNumber: Int)=
+    suspend fun getHeadLines(countryCode: String, pageNumber: Int) =
         RetrofitInstance.api.getHeadLines(countryCode,pageNumber)
 
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery,pageNumber)
 
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
 }
