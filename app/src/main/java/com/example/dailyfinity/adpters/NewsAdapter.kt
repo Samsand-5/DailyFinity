f4a85bcb326096b61ecff03009a11b97b6475334
coder <soumyodipsutra@gmail.com>
2024-05-15 08:25:14 +0530

    making adapter class

--- app/src/main/java/com/example/dailyfinity/adpters/NewsAdapter.kt
+++ app/src/main/java/com/example/dailyfinity/adpters/NewsAdapter.kt
@@ -1,4 +1,31 @@
 package com.example.dailyfinity.adpters
 
-class NewsAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        lateinit var articleImage: ImageView
+        lateinit var articleSource: TextView
+        lateinit var articleTitle: TextView
+        lateinit var articleDescription: TextView
+        lateinit var articleDateTime: TextView
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
