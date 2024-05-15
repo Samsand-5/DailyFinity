5f31f19cdc634a199eebcb48b558db396445c415
coder <soumyodipsutra@gmail.com>
2024-05-15 09:00:58 +0530

    making adapter class

--- app/src/main/java/com/example/dailyfinity/adpters/NewsAdapter.kt
+++ app/src/main/java/com/example/dailyfinity/adpters/NewsAdapter.kt
@@ -1,24 +1,26 @@
 package com.example.dailyfinity.adpters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.dailyfinity.R
 
 class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
-
     inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         lateinit var articleImage: ImageView
         lateinit var articleSource: TextView
         lateinit var articleTitle: TextView
         lateinit var articleDescription: TextView
         lateinit var articleDateTime: TextView
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        TODO("Not yet implemented")
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
+        )
     }
 
     override fun getItemCount(): Int {
