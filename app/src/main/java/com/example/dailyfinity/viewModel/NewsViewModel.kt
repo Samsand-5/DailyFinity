bef11797fed19b07d5fdff53049f31378479a5c1
coder <soumyodipsutra@gmail.com>
2024-05-16 23:43:15 +0530

    viewmodel crreating

---
+++ app/src/main/java/com/example/dailyfinity/viewModel/NewsViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.dailyfinity.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import com.example.dailyfinity.models.NewsResponse
+import com.example.dailyfinity.util.Resource
+
+class NewsViewModel {
+
+    val headLines: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+
+}
