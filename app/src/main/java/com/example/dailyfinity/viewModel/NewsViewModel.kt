575f13717b8c5da23c8b0d91b7554f3d6fe6a7de
coder <soumyodipsutra@gmail.com>
2024-05-16 23:46:17 +0530

    creating viewmodel which stores data values when screen rotation happens

--- app/src/main/java/com/example/dailyfinity/viewModel/NewsViewModel.kt
+++ app/src/main/java/com/example/dailyfinity/viewModel/NewsViewModel.kt
@@ -9,4 +9,8 @@ class NewsViewModel {
     val headLines: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var headLinesPage = 1
     var headLinesResponse: NewsResponse? = null
+
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val searchNewsPAge = 1
+    val searchNewsResponse: NewsResponse? = null
 }
