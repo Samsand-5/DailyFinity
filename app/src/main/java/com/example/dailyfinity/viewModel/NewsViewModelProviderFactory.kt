a8f687b07d814c879653dfad38a969d63858e494
coder <soumyodipsutra@gmail.com>
2024-05-18 20:41:02 +0530

    created NewsViewModelProviderFactory

--- app/src/main/java/com/example/dailyfinity/viewModel/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/example/dailyfinity/viewModel/NewsViewModelProviderFactory.kt
@@ -2,9 +2,13 @@ package com.example.dailyfinity.viewModel
 
 import android.app.Application
 import android.widget.ViewSwitcher.ViewFactory
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.dailyfinity.repository.NewsRepository
 
 class NewsViewModelProviderFactory(val app: Application, val newsRepository: NewsRepository): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NewsViewModel(app,newsRepository) as T
+    }
 
 }
