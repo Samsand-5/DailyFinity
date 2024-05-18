6045c3086087944b6f1235c3f8205cb87dc1444f
coder <soumyodipsutra@gmail.com>
2024-05-18 22:31:15 +0530

    making mainActivity

--- app/src/main/java/com/example/dailyfinity/ui/NewsActivity.kt
+++ app/src/main/java/com/example/dailyfinity/ui/NewsActivity.kt
@@ -2,12 +2,17 @@ package com.example.dailyfinity.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.dailyfinity.R
 import com.example.dailyfinity.databinding.ActivityNewsBinding
+import com.example.dailyfinity.db.ArticleDatabase
+import com.example.dailyfinity.repository.NewsRepository
 import com.example.dailyfinity.viewModel.NewsViewModel
+import com.example.dailyfinity.viewModel.NewsViewModelProviderFactory
 
 class NewsActivity : AppCompatActivity() {
 
@@ -18,6 +23,11 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application,newsRepository)
+        newsViewModel = ViewModelProvider(this,viewModelProviderFactory).get(newsViewModel::class.java)
 
         navController = findNavController(R.id.newsNavHostFragment)
         setupActionBarWithNavController(navController)
