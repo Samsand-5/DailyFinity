5b7b3828b29dd08569d406a154a87deca5f54e3a
coder <soumyodipsutra@gmail.com>
2024-05-20 18:36:09 +0530

    making ArticleFragment.kt

--- app/src/main/java/com/example/dailyfinity/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/ArticleFragment.kt
@@ -1,12 +1,11 @@
 package com.example.dailyfinity.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.dailyfinity.R
+import com.example.dailyfinity.databinding.FragmentArticleBinding
 import com.example.dailyfinity.viewModel.NewsViewModel
 
 
@@ -14,5 +13,11 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
 
     lateinit var newsViewModel: NewsViewModel
     val args: ArticleFragmentArgs by navArgs()
+    lateinit var binding: FragmentArticleBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentArticleBinding.bind(view)
+    }
 
 }
