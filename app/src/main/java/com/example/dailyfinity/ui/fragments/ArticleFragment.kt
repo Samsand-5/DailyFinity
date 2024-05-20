da312df35fe4ec10d0ac8c227b7fe923555bf3ea
coder <soumyodipsutra@gmail.com>
2024-05-20 18:34:24 +0530

    making ArticleFragment.kt

--- app/src/main/java/com/example/dailyfinity/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/ArticleFragment.kt
@@ -5,21 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.dailyfinity.R
+import com.example.dailyfinity.viewModel.NewsViewModel
 
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
-    }
+    lateinit var newsViewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
 
 }
