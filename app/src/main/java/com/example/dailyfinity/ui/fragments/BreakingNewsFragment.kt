94afb26d183cda25f906f824ec9f69181fb664f0
coder <soumyodipsutra@gmail.com>
2024-05-20 18:55:58 +0530

    making BreakingNewsFragment.kt

--- app/src/main/java/com/example/dailyfinity/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/BreakingNewsFragment.kt
@@ -5,23 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.dailyfinity.R
+import com.example.dailyfinity.adapters.NewsAdapter
+import com.example.dailyfinity.viewModel.NewsViewModel
 
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_breaking_news, container, false)
-    }
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorText: TextView
+    lateinit var itemHeadlinesError: CardView
 
 }
