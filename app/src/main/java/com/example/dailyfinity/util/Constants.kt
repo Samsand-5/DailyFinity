dd672d5569bfb994a89416066f62eba91ea299a4
coder <soumyodipsutra@gmail.com>
2024-05-21 13:06:03 +0530

    some errors resolved

--- app/src/main/java/com/example/dailyfinity/util/Constants.kt
+++ app/src/main/java/com/example/dailyfinity/util/Constants.kt
@@ -5,6 +5,6 @@ class Constants {
         const val API_KEY = "b3a51abb898d41ac8584930d4f30928e"
         const val BASE_URL = "https://newsapi.org/"
         const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val QUERY_PAGE_SIZE = 20
     }
 }
