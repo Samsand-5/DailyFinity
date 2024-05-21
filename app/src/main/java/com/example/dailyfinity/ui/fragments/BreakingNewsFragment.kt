a98c3bba0618a843a42fec37e571977e03acfcba
coder <soumyodipsutra@gmail.com>
2024-05-21 13:09:32 +0530

    made onScrollStateChanged function

--- app/src/main/java/com/example/dailyfinity/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/BreakingNewsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Button
 import android.widget.TextView
 import androidx.cardview.widget.CardView
@@ -87,6 +88,10 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
+
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
         }
     }
 }
