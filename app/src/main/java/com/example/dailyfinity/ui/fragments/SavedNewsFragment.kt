9ef88dd12ffe9b6528ade0f7098fcf5ad3304dd8
coder <soumyodipsutra@gmail.com>
2024-05-23 10:40:34 +0530

    making SavedNewsFragment.kt

--- app/src/main/java/com/example/dailyfinity/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/SavedNewsFragment.kt
@@ -5,22 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dailyfinity.R
+import com.example.dailyfinity.adapters.NewsAdapter
+import com.example.dailyfinity.databinding.FragmentBreakingNewsBinding
+import com.example.dailyfinity.databinding.FragmentSavedNewsBinding
+import com.example.dailyfinity.viewModel.NewsViewModel
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var binding: FragmentSavedNewsBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+        binding = FragmentSavedNewsBinding.bind(view)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_news, container, false)
+    private fun setUpRecyclerView(){
+        newsAdapter = NewsAdapter()
+        binding.recyclerFavourites.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 
 }
