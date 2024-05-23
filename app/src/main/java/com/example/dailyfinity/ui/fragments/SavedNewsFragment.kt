b78a9255b6c19db7f0e95860e605544362940fe4
coder <soumyodipsutra@gmail.com>
2024-05-23 10:44:01 +0530

    making SavedNewsFragment.kt

--- app/src/main/java/com/example/dailyfinity/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/SavedNewsFragment.kt
@@ -1,15 +1,18 @@
 package com.example.dailyfinity.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dailyfinity.R
 import com.example.dailyfinity.adapters.NewsAdapter
 import com.example.dailyfinity.databinding.FragmentBreakingNewsBinding
 import com.example.dailyfinity.databinding.FragmentSavedNewsBinding
+import com.example.dailyfinity.ui.NewsActivity
 import com.example.dailyfinity.viewModel.NewsViewModel
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
@@ -21,6 +24,16 @@ class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentSavedNewsBinding.bind(view)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        setUpRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article",it)
+            }
+            findNavController().navigate(R.id.action_savedNewsFragment_to_articleFragment,bundle)
+        }
     }
 
     private fun setUpRecyclerView(){
