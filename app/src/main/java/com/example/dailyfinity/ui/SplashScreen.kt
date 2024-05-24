0ebabfe1604b6c81c308a3b309ac9b56e664069a
coder <soumyodipsutra@gmail.com>
2024-05-24 22:37:48 +0530

    making a SplashScreen

--- app/src/main/java/com/example/dailyfinity/ui/SplashScreen.kt
+++ app/src/main/java/com/example/dailyfinity/ui/SplashScreen.kt
@@ -1,12 +1,19 @@
 package com.example.dailyfinity.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.dailyfinity.R
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, NewsActivity::class.java))
+        },2000)
     }
 }
