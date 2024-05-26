ce166a55c8a218f33fec47b27ccc128612656284
coder <soumyodipsutra@gmail.com>
2024-05-26 23:50:17 +0530

    making SearchNewsFragment.kt

--- app/src/main/java/com/example/dailyfinity/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/SearchNewsFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
 import com.example.dailyfinity.R
 import com.example.dailyfinity.adapters.NewsAdapter
 import com.example.dailyfinity.databinding.FragmentSavedNewsBinding
 import com.example.dailyfinity.databinding.FragmentSearchNewsBinding
+import com.example.dailyfinity.ui.NewsActivity
 import com.example.dailyfinity.viewModel.NewsViewModel
 
 
@@ -16,6 +19,16 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
 
     lateinit var newsViewModel: NewsViewModel
     lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorText: String
+    lateinit var itemSearchError: CardView
     lateinit var binding: FragmentSearchNewsBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSearchNewsBinding.bind(view)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+    }
 }
