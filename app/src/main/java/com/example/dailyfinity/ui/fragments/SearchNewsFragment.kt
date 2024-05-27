655c560818e5fc730e21f6c90596f47417620b8f
coder <soumyodipsutra@gmail.com>
2024-05-27 10:37:45 +0530

    making SearchNewsFragment.kt

--- app/src/main/java/com/example/dailyfinity/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/SearchNewsFragment.kt
@@ -6,14 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Button
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.dailyfinity.R
 import com.example.dailyfinity.adapters.NewsAdapter
 import com.example.dailyfinity.databinding.FragmentSavedNewsBinding
 import com.example.dailyfinity.databinding.FragmentSearchNewsBinding
 import com.example.dailyfinity.ui.NewsActivity
+import com.example.dailyfinity.util.Constants
 import com.example.dailyfinity.viewModel.NewsViewModel
 
 
@@ -63,5 +67,38 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
         itemSearchError.visibility = View.VISIBLE
         errorText.text = message
         isError = true
+    }
+
+    val scrollListener = object: RecyclerView.OnScrollListener(){
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNoError = !isError
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= Constants.QUERY_PAGE_SIZE
+            val shouldPaginate = isNoError && isNotLoadingAndNotLastPage && isAtLastItem
+                    && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+
+            if(shouldPaginate){
+                newsViewModel.getHeadLines("us")
+                isScrolling = false
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+
+            if(newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                isScrolling = true
+            }
+        }
     }
 }
