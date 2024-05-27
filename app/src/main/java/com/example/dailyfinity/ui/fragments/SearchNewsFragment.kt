178e4b5d8ff9f4e47629f6e756da9f000b48c8a9
coder <soumyodipsutra@gmail.com>
2024-05-27 10:36:40 +0530

    making SearchNewsFragment.kt

--- app/src/main/java/com/example/dailyfinity/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/dailyfinity/ui/fragments/SearchNewsFragment.kt
@@ -1,11 +1,13 @@
 package com.example.dailyfinity.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import com.example.dailyfinity.R
 import com.example.dailyfinity.adapters.NewsAdapter
@@ -20,16 +22,46 @@ class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
     lateinit var newsViewModel: NewsViewModel
     lateinit var newsAdapter: NewsAdapter
     lateinit var retryButton: Button
-    lateinit var errorText: String
+    lateinit var errorText: TextView
     lateinit var itemSearchError: CardView
     lateinit var binding: FragmentSearchNewsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding = FragmentSearchNewsBinding.bind(view)
 
-        newsViewModel = (activity as NewsActivity).newsViewModel
+       /* itemSearchError = view.findViewById(R.id.itemSearchError)
+
+        val inflater = requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflater.inflate(R.layout.item_error,null)
+
+        retryButton = view.findViewById(R.id.retryButton)
+        errorText = view.findViewById(R.id.errorText) */
     }
 
+    var isError = false
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    private fun hideProgressBar(){
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
+    }
+
+    private fun showProgressBar(){
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = true
+    }
+
+    private fun hideErrorMessage(){
+        itemSearchError.visibility = View.INVISIBLE
+        isError = false
+    }
+
+    private fun showErrorMessage(message: String){
+        itemSearchError.visibility = View.VISIBLE
+        errorText.text = message
+        isError = true
+    }
 }
